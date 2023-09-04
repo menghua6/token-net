00d2c6d83e35a303b88d20bb5de55aa929893055
yimenghua <yimenghua>
2023-09-04 15:52:30 +0800

    introduce

--- api/introduction.go
+++ api/introduction.go
@@ -1,5 +1,16 @@
 package api
 
 func Introduction() (string, error) {
-	return "欢迎来到令牌\n", nil
+	return "口令交流网 \n" + 
+	"\n" +
+	"创建需要的口令（示例）\n" +
+	"创建限次口令 /token/10 \n" +
+	"创建限时口令 /token/2023-09-21+00:00 \n" +
+	"创建限时口令 /token/1h1m \n" +
+	"\n" +
+	"查看口令会话（示例） \n" +
+	"/c31f9d6f28e243c1bef052ab46ea4b58a6d44d2505484a758a02f0dd2a63d82c \n" +
+	"\n" +
+	"发送口令会话（示例）\n" +
+	"/c31f9d6f28e243c1bef052ab46ea4b58a6d44d2505484a758a02f0dd2a63d82c/测试 \n", nil
 }
--- api/sendMessage.go
+++ api/sendMessage.go
@@ -13,7 +13,17 @@ func SendMessage(token string, message string) (string, error) {
 	if !checkLimit(tokenInfo) {
 		return "", errors.New("token has expired")
 	}
+	if tokenInfo.Count > 0 {
+		tokenInfo.Count = tokenInfo.Count - 1
+		err = db.UpdateToken(tokenInfo)
+	}
+	if err != nil {
+		return "", err
+	}
 	err = db.CreateMessage(token, message)
+	if err != nil {
+		return "", err
+	}
 	messages, err := db.GetMessages(token)
 	if err != nil {
 		return "", err
